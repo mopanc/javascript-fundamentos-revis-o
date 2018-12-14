2b652ea603170066f7a03702d29bbe4d09b557aa
Jorge Morais <jorgemorais@iMac-de-Jorge.home>
2018-12-14 21:57:26 +0000

    params

---
+++ funcao/paramsvariaveis.js
@@ -0,0 +1,18 @@
+function soma() {
+    let soma = 0
+    for (i in arguments) {
+        soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1.1, 2.2, 3.3))
+
+console.log(soma(1.1, 2.2, 3.3, ' teste'))
+console.log(soma('a', 'b', 'c'))
+
+
+
+
