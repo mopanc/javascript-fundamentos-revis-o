0cc3d46f32403cc1ae176456c465459792439138
Jorge Morais <jorgemorais@iMac-de-Jorge.home>
2019-06-30 19:45:58 +0100

    dom 1945

---
+++ node/temporizador.js
@@ -0,0 +1,19 @@
+const schedule = require('node-schedule')
+
+const tarefa1 = schedule.scheduleJob('*/5 * 19 * * 0', function (){
+    console.log('Executando Tarefa 1!', new Date().getSeconds())
+})
+
+setTimeout(function () {
+    tarefa1.cancel()
+    console.log('Cancelando a Tarefa 1!')
+}, 20000)
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(0, 4)]
+regra.hour = 19
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function () {
+    console.log('Executando Tarefa2!', new Date().getSeconds())
+})
